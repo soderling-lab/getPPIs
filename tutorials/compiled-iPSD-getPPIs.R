e7c754d75a0a1922b4ab4469a102398d14d2abf5
twesleyb <twesleyb10@gmail.com>
2021-03-03 16:52:34 -0500

    add tutorial

--- tutorials/compiled-iPSD-getPPIs.R
+++ tutorials/compiled-iPSD-getPPIs.R
@@ -1,10 +1,11 @@
 #!/usr/bin/env Rscript
 
 library(dplyr)
+library(igraph)
 library(data.table)
 
-library(igraph)
 library(getPPIs)
+library(geneLists)
 
 data(ciPSD) # geneLists
 entrez <- unique(unlist(ciPSD))
@@ -17,8 +18,11 @@ data(list=c("mouse","human","rat"), package="getPPIs") # taxids
 edge_df <- musInteractome %>% 
 	filter(Interactor_A_Taxonomy %in% c(mouse,human,rat)) %>% 
 	filter(osEntrezA %in% entrez & osEntrezB %in% entrez) %>%
-	select(osEntrezA, osEntrezB, Publications)
+	select(osEntrezA, osEntrezB, Publications) %>%
+	mutate(osEntrezA=paste0("entrez",osEntrezA)) %>%
+	mutate(osEntrezB=paste0("entrez",osEntrezB))
 
+# ^ row and column indices should be char!
 
 g <- simplify(graph_from_data_frame(edge_df))
 
